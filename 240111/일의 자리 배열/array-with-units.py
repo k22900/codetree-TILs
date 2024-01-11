15e2e753ba76e3cfe4e16435222a256c89fd841c
k22900 <132814310+k22900@users.noreply.github.com>
2024-01-11 13:01:56 +0900

    Title: 일의 자리 배열, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240111/일의 자리 배열/array-with-units.py
@@ -0,0 +1,8 @@
+arr=list(map(int,input().split()))
+# for i in range(2,10):
+#     num=arr[i-2]+arr[i-1]#
+#     if  num>=10:
+#         num-=10
+#     arr.append(num)
+# for i in range(10):
+#     print(arr[i],end=" ")
