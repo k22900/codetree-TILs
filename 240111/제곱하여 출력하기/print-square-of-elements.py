44124720106316671858320d8255d0f226563103
k22900 <132814310+k22900@users.noreply.github.com>
2024-01-11 12:45:37 +0900

    Title: 제곱하여 출력하기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/제곱하여 출력하기/print-square-of-elements.py
+++ 240111/제곱하여 출력하기/print-square-of-elements.py
@@ -1,5 +1,8 @@
 num1=int(input())
 arr=list(map(int,input().split()))
-for i in range(num1):
-    arr[i]=arr[i]*arr[i]
-    print(arr[i],end=" ")
+# for i in range(num1):
+#     arr[i]=arr[i]*arr[i]
+#     print(arr[i],end=" ")
+res=[e*e for e in arr]
+for e in res:
+    print(e,end=" ")
