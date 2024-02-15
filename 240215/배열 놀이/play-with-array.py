1fadd97518cb85355404c326ca683b2d46332145
k22900 <132814310+k22900@users.noreply.github.com>
2024-02-15 21:32:07 +0900

    Title: 배열 놀이, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240215/배열 놀이/play-with-array.py
+++ 240215/배열 놀이/play-with-array.py
@@ -15,10 +15,10 @@ num=list(map(int,input().split()))#n개의 원소를 받는 배열num
 
 for _ in range(q):
     task=list(map(int,input().split()))
-    if task[0]==1:
+    if task[0]==1:#a번쨰 원소출력
         ans=num[task[1]-1]
         print(ans)    
-    elif task[0]==2:
+    elif task[0]==2:#a가 몇번째 
         ans=findNum(task[1])
         print(ans+1)
     elif task[0]==3:
