3533831ac1f9cabb55e25811b40e236eb5bc2c62
k22900 <132814310+k22900@users.noreply.github.com>
2024-02-15 21:04:33 +0900

    Title: 배열 놀이, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/배열 놀이/play-with-array.py
@@ -0,0 +1,27 @@
+def findNum(n1):
+    for idx,el in enumerate(num):
+        if el==n1:
+            return idx
+    return 0
+
+def nums(n1,n2):
+    num_list=[]
+    for i in range(n1-1,n2):
+        num_list.append(num[i])
+    return num_list
+
+n,q=map(int,input().split())#원소가 주어지는지 개수를 알려주는n,질의의 개수를 알리는q
+num=list(map(int,input().split()))#n개의 원소를 받는 배열num
+
+for _ in range(q):
+    task=list(map(int,input().split()))
+    if task[0]==1:
+        ans=num[task[1]-1]
+        print(ans)    
+    elif task[0]==2:
+        ans=findNum(task[1])
+        print(ans+1)
+    elif task[0]==3:
+        ans=nums(task[1],task[2])
+        for i in range(len(ans)):
+            print(ans[i],end=" ")
