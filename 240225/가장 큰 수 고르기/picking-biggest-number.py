5e90c5040868dda4a70d727be3080c0f326267e8
k22900 <132814310+k22900@users.noreply.github.com>
2024-02-25 18:29:10 +0900

    Title: 가장 큰 수 고르기, Time: 247ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240225/가장 큰 수 고르기/picking-biggest-number.py
@@ -0,0 +1,10 @@
+nums=list(map(int,input().split()))
+maxNum=0
+for num in nums:
+    if num>=maxNum:
+        maxNum=num
+print(maxNum)
+# 내가 뭘했는지 물어보고 
+# 내가 뭘했는지 답하고
+# 내가 뭘해야되는지 물어보고
+# 내가뭘할건지 대답하자
