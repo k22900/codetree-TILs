8b7765f6574fd290bcd501155fe9dd908297a320
k22900 <132814310+k22900@users.noreply.github.com>
2024-02-25 19:30:01 +0900

    Title: 999 또는 -999, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/999 또는 -999/999-or-999.py
+++ 240225/999 또는 -999/999-or-999.py
@@ -1,10 +1,10 @@
 nums=list(map(int,input().split()))
 res_list=[]
 for elm in nums:
-    if elm>-999 and elm< 999:
-        res_list.append(elm)
-    else:
+    if elm==-999 and elm== 999:
         break
+    res_list.append(elm)
+
 res_min=min(res_list)
 res_max=max(res_list)
 print(res_max,res_min)
